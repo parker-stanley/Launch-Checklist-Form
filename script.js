1eec64050681818bd0c3e70295140cc7ff46a75d
Parker Stanley <parker.stanley@SOMO-M-DC-005.local>
2020-09-08 17:00:26 -0400

    first commit that includes empty field errors

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 // Write your JavaScript code here!
 
+window.addEventListener("load", function() {
+   let button = document.getElementById("formSubmit");
+   button.addEventListener("click", function(){
+      let pilotName = document.getElementById("pilotName");
+      let copilotName = document.querySelector("input[name=copilotName]");
+      let fuelLEvel = document.querySelector("input[name=fuelLevel]")
+      let cargoMass = document.querySelector("input[name=cargoMass]");
+      if (pilotName == "" || copilotName == "" || fuelLEvel == "" || cargoMass == ""){
+         alert("Please enter valid information into required fields.")
+      }
+   })
+}
+
+
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
 <ol>
