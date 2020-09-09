3c3ebe7b1b12f7c9fd76b957bf6e87437f7a920b
Parker Stanley <parker.stanley@SOMO-M-DC-005.local>
2020-09-08 22:21:28 -0400

    finished the logic to fetch the missionTarget div population:

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 // Write your JavaScript code here!
 
+fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response){
+   response.json().then( function(json) {
+      let destination = document.getElementById("missionTarget");
+      destination.innerHTML = `
+      <h2>Mission Destination</h2>
+      <ol>
+         <li>Name: ${json[0].name}</li>
+         <li>Diameter: ${json[0].diameter}</li>
+         <li>Star: ${json[0].star}</li>
+         <li>Distance from Earth: ${json[0].distance}</li>
+         <li>Number of Moons: ${json[0].moons}</li>
+      </ol>
+      <img src="${json[0].image}"> `
+   });
+});
+
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
 <ol>
