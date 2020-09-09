a1d6837f08ae73c77f9e6eb8f2072e7aaf5cb51f
Parker Stanley <parker.stanley@SOMO-M-DC-005.local>
2020-09-08 22:46:28 -0400

    cleaned up code to remove comments

--- script.js
+++ script.js
@@ -16,16 +16,6 @@ fetch("https://handlers.education.launchcode.org/static/planets.json").then(func
    });
 });
 
-/* This block of code shows how to format the HTML once you fetch some planetary JSON!
-<h2>Mission Destination</h2>
-<ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
-</ol>
-<img src="${}"> */
 
 window.addEventListener("load", function() {
    let button = document.getElementById("formSubmit");
